4c56430d252e256dad5d8ae79933d3fa60598b35
pado34 <padoantoine@gmail.com>
2026-03-15 21:58:01 +0100

    cleaning up code

--- examensms.py
+++ examensms.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
 from selenium.webdriver.common.action_chains import ActionChains
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+
+from bs4 import BeautifulSoup
+
+from twilio.rest import Client
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized");
@@ -15,7 +17,6 @@ options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
 driver = webdriver.Chrome(r"votrecheminc\chromedriver.exe",chrome_options=options)
 driver.get("https://cas.umontpellier.fr/cas/login")
-    
 
 username = driver.find_element_by_name("username")
 password = driver.find_element_by_name("password")
@@ -24,42 +25,36 @@ geolocation = driver.find_element_by_name("geolocation")
 _eventId = driver.find_element_by_name("_eventId")
 submit = driver.find_element_by_name("submit")
 
-
+#Input mail and pwd here:
 username.send_keys("adressemail")
 password.send_keys("motdepasseent")
 
+#Input phone number, twilio number 
+phone_number = '+votrenumero'
+twilio_number = '+numerotwilio'
 
-time.sleep(7)#7 before
+#Input twilio token and sid
+twilio_token = 'tokentwilio'
+twilio_sid = 'sidtwilio'
 
+time.sleep(7)
 submit.click()
 
-
 driver.get("https://app.umontpellier.fr/mdw/#!notesView")
+time.sleep(5)
 
-time.sleep(5)#5b4
+span_notes = driver.find_elements_by_xpath('//span[text()="Notes & résultats"]')
+print(span_notes)
+span_notes[0].click()
+time.sleep(5)
 
-reee= driver.find_elements_by_xpath('//span[text()="Notes & résultats"]')
-print(reee)
-reee[0].click()
-time.sleep(5)#10b4
+span_m1 = driver.find_elements_by_xpath('//span[text()="M1 Physique Numérique"]')
+driver.execute_script("arguments[0].click();", span_m1[0]) 
+time.sleep(5)
 
-
-
-
-eeeeeeeeeeee= driver.find_elements_by_xpath('//span[text()="M1 Physique Numérique"]')
-
-
-
-driver.execute_script("arguments[0].click();", eeeeeeeeeeee[0]) 
-time.sleep(5)#5b4
-
-dddddd= driver.find_elements_by_xpath('//span[text()="Fermer"]')
-
-driver.execute_script("arguments[0].click();", dddddd[0]) 
-
-time.sleep(5)#5b4
-
-from bs4 import BeautifulSoup
+span_fermer = driver.find_elements_by_xpath('//span[text()="Fermer"]')
+driver.execute_script("arguments[0].click();", span_fermer[0])
+time.sleep(5)
 
 soup = BeautifulSoup(driver.page_source,features="lxml")
 
@@ -73,18 +68,12 @@ for res in result:
         print(res.text)
         thesms+=res.text
         thesms+=" | "
-    
-
-
-
-from twilio.rest import Client
-
 
 try:
     f = open(r"votrecheminc\marks.txt", "r")
     a=f.read()
     if a==thesms:
-        print("do nada")
+        print("do nothing")
         f.close()
     else:
         print("do something")
@@ -93,15 +82,15 @@ try:
         f.write(thesms)
         f.close()
 
-        account_sid = 'sidtwilio'
-        auth_token = 'tokentwilio'
+        account_sid = twilio_sid
+        auth_token = twilio_token
         client = Client(account_sid, auth_token)
 
         message = client.messages \
             .create(
                  body=thesms,
-                 from_='+numerotwilio',
-                 to='+votrenumero'
+                 from_=twilio_number,
+                 to=phone_number
              )
 
 except:
@@ -110,18 +99,16 @@ except:
     f.write(thesms)
     f.close()
 
-    account_sid = 'sidtwilio'
-    auth_token = 'tokentwilio'
+    account_sid = twilio_sid
+    auth_token = twilio_token
     client = Client(account_sid, auth_token)
 
     message = client.messages \
         .create(
              body=thesms,
-             from_='+numerotwilio',
-             to='+votrenumero'
+             from_=twilio_number,
+             to=phone_number
          )
-
-
 
 time.sleep(12)
 driver.quit()
